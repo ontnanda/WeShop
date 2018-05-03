78ad46c3cfb530b4e7cb6370b99d80a83fa1f6b2
Surapong Kanchanarate <SurapongK@Ontnanda.local>
2018-05-03 13:57:36 +0700

    add store goods detail page

--- config/api.js
+++ config/api.js
@@ -8,6 +8,7 @@ module.exports = {
 
   GoodsCount: ApiRootUrl + 'goods/count',  //统计商品总数
   GoodsList: ApiRootUrl + 'goods/list',  //获得商品列表
+  GoodsListMenus: ApiRootUrl + 'goods/listgood',  //获得商品列表
   GoodsCategory: ApiRootUrl + 'goods/category',  //获得分类数据
   GoodsDetail: ApiRootUrl + 'goods/detail',  //获得商品的详情
   GoodsNew: ApiRootUrl + 'goods/new',  //新品
